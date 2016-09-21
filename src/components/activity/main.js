f91a3d282876d1b0b8ea2f64fad4a7ade23e642a
taoveweb <taoveweb@gmail.com>
2016-09-21 14:55:40 +0800

    rooute

--- src/components/activity/main.js
+++ src/components/activity/main.js
@@ -10,14 +10,24 @@ class Main extends React.Component {
     super(props, context);
   }
 
+  componentDidMount(){
+    console.log('asfasfa');
+    console.log(this.props);
+  }
+
   render() {
+
     return (
       <div>
-        <h1>React Slingshot</h1>
+        <h1>React Slingshotsfasfasdf</h1>
 
         <h2>Get Started</h2>
         <ol>
           <li>Review the <Link to="fuel-savings">demo app</Link></li>
+          <li>Remove the demo and start coding: npm run remove-demo</li>
+          <li>Remove the demo and start coding: npm run remove-demo</li>
+          <li>Remove the demo and start coding: npm run remove-demo</li>
+          <li>Remove the demo and startsadfasfafasf coding: npm run remove-demo</li>
           <li>Remove the demo and start coding: npm run remove-demo</li>
         </ol>
       </div>
